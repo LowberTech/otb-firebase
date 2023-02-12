2096668b0fa0f160c9d5b475d8127eaad5458d48
LowberTech <91351615+LowberTech@users.noreply.github.com>
2023-02-11 20:11:28 -0700

    upload src/app/config.js

---
+++ src/app/config.js
@@ -0,0 +1,13 @@
+// Firebase configuration
+const config = {
+    apiKey: "AIzaSyCjFSMXAF0-ySQ7Uh7oWhzboEHawpWxSI0",
+    authDomain: "off-the-bench.firebaseapp.com",
+    databaseURL: "https://off-the-bench-default-rtdb.firebaseio.com",
+    projectId: "off-the-bench",
+    storageBucket: "off-the-bench.appspot.com",
+    messagingSenderId: "1063527538854",
+    appId: "1:1063527538854:web:b29ffe8d634d17c03bd467",
+    measurementId: "G-3CSCQWS4NP"
+};
+
+module.exports = config;
